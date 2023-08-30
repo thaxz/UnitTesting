da7fde806966678118dc7bc2fd276a6a034a7e78
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-08-30 17:18:23 -0300

    first commit

---
+++ UnitTesting/UnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingApp.swift
+//  UnitTesting
+//
+//  Created by thaxz on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
